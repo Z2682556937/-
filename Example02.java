ce504be317a468e962c1c97a3b8ffaaf22eeecfa
Z2682556937 <55043288+Z2682556937@users.noreply.github.com>
2019-09-22 23:10:42 +0800

    Add files via upload

--- Example02.java
+++ Example02.java
@@ -1,15 +1,21 @@
-class Father{
-	private String name="zhangjun";
-	class Child{
-		void introFather() {
-			System.out.println("My Father is "+name);
-		}
+// 定义Animal类
+class Animal {		
+    //定义动物叫的方法		
+	void shout() {			  
+		System.out.println("动物发出叫声");
 	}
-	
 }
-class Example02{
+// 定义Dog类继承动物类
+class Dog extends Animal {    
+	// 定义狗叫的方法
+	void shout() {			 
+		System.out.println("汪汪……");
+	}
+}
+// 定义测试类
+public class Example02 {	
 	public static void main(String[] args) {
-		Father.Child ch=new Father().new Child();
-		ch.introFather();
+		Dog dog = new Dog(); // 创建Dog类的实例对象
+		dog.shout();           // 调用dog重写的shout()方法
 	}
 }
