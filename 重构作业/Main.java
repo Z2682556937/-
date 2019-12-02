5e1090072cdbc06b99f57ea1b1f9eaccf8c4d738
Z2682556937 <55043288+Z2682556937@users.noreply.github.com>
2019-12-02 22:22:48 +0800

    重构：电影票

---
+++ 重构作业/Main.java
@@ -0,0 +1,20 @@
+package chonggou;
+//import movie.*;
+//import customer.*;
+//import rental.*;
+import java.util.*;
+public class Main {
+  public static void main(String [] args) {
+	  Movie movie1 = new Movie("中国机长",0);
+	  Movie movie2 = new Movie("冰雪奇缘   ",1); 
+	  Movie movie3 = new Movie("肖申克的救赎 ",2); 
+	  Rental rental1 = new Rental(movie1,10); 
+	  Rental rental2 = new Rental(movie2,5);
+	  Rental rental3 = new Rental(movie3,6);
+	  Customer customer = new Customer("涂**"); 
+	  customer.addRental(rental1);
+	  customer.addRental(rental2);  
+	  customer.addRental(rental3);  
+	  System.out.println(customer.statment());
+  }
+}
