55fc9741271c776ac0ed04450a388205a9d6da91
Z2682556937 <55043288+Z2682556937@users.noreply.github.com>
2019-12-03 22:31:45 +0800

    Add files via upload

---
+++ PBL（8）--最终版/Price.java
@@ -0,0 +1,13 @@
+package chonggou;
+
+abstract class Price {
+	abstract int getPriceCode();
+	abstract double getCharge(int daysRented);
+
+	int getFrequentRenterPoints(int daysRented) {
+
+			return 1;
+		}
+		
+
+}
